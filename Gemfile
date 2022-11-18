82b6e910c65a78f61b3351f7c3878fc54211dda2
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2022-11-18 16:14:18 +0100

    added the add picture functionality

--- Gemfile
+++ Gemfile
@@ -50,6 +50,9 @@ gem "autoprefixer-rails"
 gem "font-awesome-sass", "~> 6.1"
 gem "simple_form", github: "heartcombo/simple_form"
 
+# Cloudinary
+gem "cloudinary"
+
 # env
 gem "dotenv-rails", groups: [:development, :test]
 
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,11 @@
 class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
+  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
+  # validates :photo, attached: true, content_type: ['photo/jpg', 'photo/jpeg', 'photo/png'],
+  #                   size: { less_than: 1.megabytes, message: 'must be smaller than 1mb' }
+  # needs the below gem
+  # gem 'active_storage_validations'
 end
