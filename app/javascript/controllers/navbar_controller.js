66f287cd9ab0d36fde71983ae2581352ec443432
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2022-11-19 11:39:09 +0100

    added js to navbar

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -5,6 +5,17 @@ export default class extends Controller {
   static targets = ['nav']
 
   connect() {
-    console.log("hi");
+    console.log("nav js connected");
+  }
+  navToggle() {
+    let pagePosition = window.scrollY
+    this.navTarget.style.opacity = 1
+    this.navTarget.style.backgroundColor = 'rgb(232, 232, 232)'
+    this.navTarget.style.color = "black"
+    if(pagePosition > 30) {
+      this.navTarget.style.opacity = 0.9
+      this.navTarget.style.backgroundColor = 'rgb(40, 42, 58)'
+      this.navTarget.style.color = "white"
+    }
   }
 }
